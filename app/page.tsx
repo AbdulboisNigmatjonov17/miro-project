bd9d6309f99f4eef0317e392e8c1f11f5b79eaf3
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-03-24 16:41:56 +0500

    eeendi navbar qismi tugadi

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,10 @@
 import { useTranslation } from "react-i18next";
 
 export default function Home() {
-  const { t } = useTranslation();
 
   return (
     <div>
-      <h2>Home</h2>
-      <h1>{t("welcome")}</h1>
+      
     </div>
   )
 }
--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,13 +1,44 @@
 "use client";
 
 import { i18n } from "@/i18n"; // Named export'ni import qilamiz
+import { ExpandMore, Language } from '@mui/icons-material';
+import Link from "next/link";
+import { useState } from "react";
+import { useTranslation } from "react-i18next";
 
+{/* <h1>{t("welcome")}</h1> */ }
 export default function Navbar() {
+  const [showBtn, setShowBtn] = useState(false)
+  const { t } = useTranslation();
   return (
-    <div className="flex gap-10 py-10 px-10">
-      
-      <button onClick={() => i18n.changeLanguage("uz")}>Uz</button>
-      <button onClick={() => i18n.changeLanguage("en")}>En</button>
-    </div>
+    <nav className="w-full h-[10vh] flex justify-between">
+      <div className="w-1/2 flex items-center justify-center gap-6">
+        <Link href={'/'}>
+          <img src="/logo.svg" alt="miro logo" className="" />
+        </Link>
+        <ul className="flex gap-4 items-center">
+          <li className="hover:underline underline-offset-4">{t("Product")} <ExpandMore /></li>
+          <li className="hover:underline underline-offset-4">{t("Solutions")} <ExpandMore /></li>
+          <li className="hover:underline underline-offset-4">{t("Resources")} <ExpandMore /></li>
+          <li className="hover:underline underline-offset-4">{t("Enterprise")}</li>
+          <li className="hover:underline underline-offset-4">{t("Pricing")}</li>
+        </ul>
+      </div>
+      <div className="w-1/2 flex justify-center items-center gap-6">
+        <button onClick={() => setShowBtn(prev => !prev)}
+          className="relative cursor-pointer">
+          <Language />{t("EN")}
+          {showBtn && <div className="absolute top-6 left-5 flex flex-col gap-1 border border-[#F0F0F0] shadow-2xs">
+            <div className="border border-[#F0F0F0] py-1 px-3 cursor-pointer" onClick={() => i18n.changeLanguage("uz")}>Uz</div>
+            <div className="border border-[#F0F0F0] py-1 px-3 cursor-pointer" onClick={() => i18n.changeLanguage("en")}>En</div>
+          </div>}
+        </button>
+        <h3>{t("Contact Sales")}</h3>
+        <div>
+          <button className="hover:bg-[#4262FF] rounded-full py-4 px-5 text-black hover:text-white cursor-pointer">{t("Sign up free")} →</button>
+          <button className="bg-[#4262FF] rounded-full py-4 px-5 text-white cursor-pointer">{t("Sign up free")} →</button>
+        </div>
+      </div>
+    </nav>
   );
 }
