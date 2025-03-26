29947b1ea140a2f43b85d10ecaf21d164a2338d8
AbdulboisNigmatjonov17 <abdulboisnigmatjonov@gmail.com>
2025-03-26 17:15:50 +0500

    4 ta componentla qoldi va keyin responsivega otaman

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,52 @@
 import Collaborate from '@/components/collaborate/Collaborate';
 import Hero from '@/components/hero/HeroBanner'
 import Logos from '@/components/logo/Logos';
+import MainSection from '@/components/sections/MainSection';
 import Section1 from '@/components/sections/Section1';
+import { Check } from '@mui/icons-material';
 import Image from 'next/image';
+import { useTranslation } from 'react-i18next';
+
+export const firstComp = () => {
+  const { t } = useTranslation()
+  return (
+    <div className='w-[350px] py-8 px-4 flex flex-col gap-6 items-start'>
+      <h4 className='text-[#050038] text-[18px] leading-6'>{t("Main Section.first comp.title")}</h4>
+      <p className='text-[#050038B2] text-[18px] leading-6'>{t("Main Section.first comp.text")}</p>
+      <button className='font-normal text-[18px] leading-6 text-[#4262FF] cursor-pointer'>{t("Section1.learn more")} →</button>
+    </div>
+  )
+}
+export const secondComp = () => {
+  const { t } = useTranslation()
+  return (
+    <div className='w-[350px] py-8 px-4 flex flex-col gap-16 items-start'>
+      <div className='w-full flex flex-col items-start gap-8'>
+        <ul className='w-full text-[#05003899] font-normal text-[18px] leading-6 flex flex-col gap-4'>
+          <li><Check /> {t("Main Section.second comp.ul.li1")}</li>
+          <li><Check /> {t("Main Section.second comp.ul.li2")}</li>
+          <li><Check /> {t("Main Section.second comp.ul.li3")}</li>
+        </ul>
+        <button className='font-normal text-[18px] leading-6 text-[#4262FF] cursor-pointer'>{t("Section1.learn more")} →</button>
+      </div>
+      <div className='w-full flex flex-col gap-3.5'>
+        <h5 className='font-normal text-[18px] leading-6 text-[#05003899]'>{t("Main Section.second comp.title")}</h5>
+        <div className='w-full flex gap-8'>
+          <Image src="/section-logos/FirstLogo.png" alt="olmos logo" width={25} height={25} />
+          <Image src="/section-logos/SecondLogo.png" alt="xd logo" width={25} height={25} />
+          <Image src="/section-logos/ThirdLogo.png" alt="figma logo" width={25} height={25} />
+          <Image src="/section-logos/FourthLogo.png" alt="qanaqadu n" width={25} height={25} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
 export default function Home() {
+  const { t } = useTranslation()
+  const firstCompData = t("Main Section.first comp.arr", { returnObjects: true }) as string[];
+  const secondCompData = t("Main Section.second comp.arr", { returnObjects: true }) as string[];
+
   return (
     <>
       <Hero />
@@ -15,6 +58,8 @@ export default function Home() {
       <section className='relative w-full h-[500px] my-10'>
         <Image src='/section.png' fill alt='Section Image' className='object-cover hover:scale-110 transition duration-200 cursor-pointer' />
       </section>
+      <MainSection title={t("Main Section.first comp.main")} data={firstCompData} img='/MainSection1.png' comp={firstComp} />
+      <MainSection title={t("Main Section.second comp.main")} data={secondCompData} img='/MainSection2.png' comp={secondComp} />
     </>
   )
 }
---
+++ components/sections/MainSection.tsx
@@ -0,0 +1,29 @@
+"use client"
+import Image from 'next/image'
+import React, { ComponentType, useState } from 'react'
+import { useTranslation } from 'react-i18next'
+
+interface MainSectionProps {
+    title: string,
+    data: string[],
+    img: string,
+    comp: ComponentType
+}
+export default function MainSection({ title, data, img, comp }: MainSectionProps) {
+    const [selectOne, setSelectOne] = useState<number | null>(null);
+    const { t } = useTranslation()
+    return (
+        <section className='w-full flex flex-col gap-8 my-20'>
+            <h1 className='font-bold text-5xl text-[#050038]'>{title}</h1>
+            <div className='w-full flex justify-between gap-2 items-center'>
+                {data.map((item: string, index: number) => (
+                    <button onClick={() => setSelectOne(index)} key={index} className={`text-[#050038] border border-[#F2F2F2] rounded-3xl py-3 pl-4 pr-7 hover:bg-[#F1F3FD] ${selectOne === index ? 'bg-[#F1F3FD]' : ''} cursor-pointer`}>{item}</button>
+                ))}
+            </div>
+            <div className='w-full flex justify-between items-start'>
+                {React.createElement(comp)}
+                <Image width={700} height={515} src={img} alt='MainSection hero image' />
+            </div>
+        </section>
+    )
+}
